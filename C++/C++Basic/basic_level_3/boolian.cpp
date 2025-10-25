e75390e291393017376295b5c0fe6ab5f8a4059c
Ringo <Ringo.kwon@gmail.com>
2025-10-26 08:54:17 +0900

    save

---
+++ C++/C++Basic/basic_level_3/boolian.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << boolalpha;
+    cout << (1 == 2) << endl;
+    cout << (1 <= 2) << endl;
+    cout << (true && false) << endl;
+    cout << (true || false) << endl;
+    return 0;
+}
