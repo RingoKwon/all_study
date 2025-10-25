9cdae5609b569e00e7d7f5310ba0a681d42cea44
Ringo <Ringo.kwon@gmail.com>
2025-10-25 19:57:15 +0900

    type

---
+++ C++/C++Basic/basic_level_1/hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    int num = 1;
+
+    std::cout << "Hello" << std::endl;
+    std::cout << "World" << std::endl;
+    std::cout << num << std::endl;
+    std::cout << num + 1 << std::endl;
+    std::cout << num + 2 << std::endl;
+    return 0;
+}
---
+++ C++/C++Basic/basic_level_1/type.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+    int num = 1;
+    double temperature = 26.4;
+    bool is_true = false;
+    std::string str = "ringo";
+
+    std::cout << num << std::endl;
+    std::cout << temperature << std::endl;
+    std::cout << is_true << std::endl;
+    std::cout << str << std::endl;
+    return 0;
+}
