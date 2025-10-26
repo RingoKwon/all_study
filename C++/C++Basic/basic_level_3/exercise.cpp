fd538537d8d2c2a02addf42d3004e87d540f25a2
Ringo <Ringo.kwon@gmail.com>
2025-10-26 11:51:18 +0900

    my solution

--- C++/C++Basic/basic_level_3/exercise.cpp
+++ C++/C++Basic/basic_level_3/exercise.cpp
@@ -32,14 +32,37 @@ int maxDynamic(vector<int> arr)
     return maxNum;
 }
 
+int maxDynamicAllocation(vector<int> arr)
+{
+    size_t i;
+
+    i = 0;
+    while (i < arr.size())
+    {
+        arr.at(i) = 1;
+        i++;
+    }
+    i = 1;
+    while (arr.at(i - 1) != 0)
+    {
+        cout << "Enter " << i + 1 << "th element: ";
+        cin >> arr.at(i);
+        i++;
+    }
+    return 0;
+}
+
 int main()
 {
     vector<int> arr = {1,2,3,4,4,4,5};
     vector<int> arrEmpty(5);
+    vector<int> arrDynamic(100);
     int maxNum;
 
     maxNum = max(arr);
     cout << "max is " << maxNum << endl;
     maxDynamic(arrEmpty);
+    maxDynamicAllocation(arrDynamic);
+
     return 0;
 }
