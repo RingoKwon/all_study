4234a1cb8a35650bc19cd94e35ef0036064b69ee
Ringo <Ringo.kwon@gmail.com>
2025-10-26 19:31:53 +0900

    exercise3 my solution :

---
+++ C++/C++Basic/basic_level_4/exercise2_reference/occur.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int cnt_occur(vector<string> &arr, string str)
+{
+    int occur = 0;
+    for (string ele: arr)
+    {
+        if (ele == str)
+            occur++;
+    }
+    return occur;
+}
+
+int main()
+{
+    int occur; 
+    vector<string> arr = {"ringo", "ringo", "bingo"};
+    occur = cnt_occur(arr, "ringo");
+    cout << occur << endl;
+    return 0;
+}
