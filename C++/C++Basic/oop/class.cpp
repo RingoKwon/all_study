4b89565b6d2a13b00cedc44cbae851dfb1711f6c
Ringo <Ringo.kwon@gmail.com>
2025-10-26 22:52:46 +0900

    class decleration

---
+++ C++/C++Basic/oop/class.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Robot
+{
+public:
+    Robot(string name, int version_number)
+        :name(name), version_number(version_number), internal_temperature(30.0)
+    {
+        // this->name = name;
+        // this->version_number = version_number;
+        // this->internal_temperature = 30.0;
+    }
+private:
+    string name;
+    int version_number;
+    double  internal_temperature;
+};
+
+int main()
+{
+    return 0;
+}
