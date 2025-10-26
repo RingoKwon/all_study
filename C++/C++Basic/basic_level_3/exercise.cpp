f6a731bcf56462b79f774c6728c090d99be93fb9
Ringo <Ringo.kwon@gmail.com>
2025-10-26 10:24:27 +0900

    why maxNum is 0 ????

---
+++ C++/C++Basic/basic_level_3/exercise.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int max(vector<int> arr)
+{
+    int max;
+
+    max = arr.at(0);
+    for (int    ele: arr)
+    {
+        if (max < ele)
+            max = ele;
+    }
+    return max;
+}
+int maxDynamic(vector<int> arr)
+{
+    int maxNum;
+    int i = 0;
+
+    for (int ele: arr)
+    {
+        
+        cout << "Enter " << i + 1 << "th Element: ";
+        cin >> ele;
+        i++;
+    }
+    maxNum = max(arr);
+    cout << "Max number is " << maxNum << endl;
+    return maxNum;
+}
+
+int main()
+{
+    vector<int> arr = {1,2,3,4,4,4,5};
+    vector<int> arrEmpty(5);
+    int maxNum;
+    maxNum = max(arr);
+    cout << "max is " << maxNum << endl;
+    maxDynamic(arrEmpty);
+    return 0;
+}
