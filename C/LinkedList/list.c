d2bc28a5fa30d0f01c3729e639f8c24a2ea3c5db
Ringo <Ringo.kwon@gmail.com>
2025-11-01 15:26:15 +0900

    infinite loop inspection start

--- C/LinkedList/list.c
+++ C/LinkedList/list.c
@@ -7,13 +7,8 @@ typedef struct node
     struct node* next;
 } Node;
 
-void prt_linked_list_recursive(Node* node)
-{
-    printf("value: %d\n", node->value);
-    if (node->next == NULL)
-        return ;
-    prt_linked_list_recursive(node->next);
-}
+void    prt_linked_list_recursive(Node* node);
+void    prt_linked_list(Node* node);
 
 int main()
 {
@@ -26,6 +21,32 @@ int main()
     b.next = &c;
     c.next = NULL;
 
+    printf("recursive: \n");
     prt_linked_list_recursive(&a);
+    printf("iterative: \n");
+    prt_linked_list(&a);
     return (0);
+}
+
+void    prt_linked_list_recursive(Node* node)
+{
+    printf("value: %d\n", node->value);
+    if (node->next == NULL)
+        return ;
+    prt_linked_list_recursive(node->next);
+}
+
+void    prt_linked_list(Node* node)
+{
+    Node*   current;
+    int i;
+
+    current = node;
+    i = 0;
+    while (current != NULL)
+    {
+        printf("Node %d: %d\n", i, current->value);
+        current = node->next;
+        i++;
+    }
 }
