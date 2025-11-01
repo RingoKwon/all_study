9c2baabf7190c7825854e74aac5691d7662a5aac
Ringo <Ringo.kwon@gmail.com>
2025-11-01 13:47:14 +0900

    print_linkedlist_recursive compeleted

--- C/LinkedList/list.c
+++ C/LinkedList/list.c
@@ -7,12 +7,12 @@ typedef struct node
     struct node* next;
 } Node;
 
-void prt_linked_list(Node* node)
+void prt_linked_list_recursive(Node* node)
 {
     printf("value: %d\n", node->value);
     if (node->next == NULL)
         return ;
-    prt_linked_list(node->next);
+    prt_linked_list_recursive(node->next);
 }
 
 int main()
@@ -26,6 +26,6 @@ int main()
     b.next = &c;
     c.next = NULL;
 
-    prt_linked_list(&a);
+    prt_linked_list_recursive(&a);
     return (0);
 }
