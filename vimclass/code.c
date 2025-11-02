9e981aac1e2e8553fc30a35b5480d9875c214e40
Ringo <Ringo.kwon@gmail.com>
2025-11-02 11:49:58 +0900

    save

---
+++ vimclass/code.c
@@ -0,0 +1,29 @@
+int selectFromWhere(void)
+{
+    return (0);
+}
+
+int main(void)
+{
+    int i;voivoidd
+        
+    p = 0;
+    return (0);
+}
+int main(void)
+{
+    int i;
+    i = 0;
+    selectFromWhere(wher)
+    {
+
+    }
+}
+
+
+
+
+
+    return (0);
+	⁃	how to 
+	⁃	how to 
