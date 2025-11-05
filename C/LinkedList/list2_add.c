a98096ba430236399a4f8fdd3c82bb1ba3c1c8ea
Ringo <Ringo.kwon@gmail.com>
2025-11-05 21:40:56 +0900

    change file name & start implemeting add function

---
+++ C/LinkedList/list2_add.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node 
+{
+    int value;
+    struct node* next;
+} Node;
+
+void    prt_linked_list_recursive(Node* node);
+void    prt_linked_list(Node* node);
+
+int main()
+{
+    Node a, b, c;
+    a.value = 1;
+    b.value = 2;
+    c.value = 3;
+
+    a.next = &b;
+    b.next = &c;
+    c.next = NULL;
+
+    printf("recursive: \n");
+    prt_linked_list_recursive(&a);
+    printf("iterative: \n");
+    prt_linked_list(&a);
+    return (0);
+}
+
+void    prt_linked_list_recursive(Node* node)
+{
+    printf("value: %d\n", node->value);
+    if (node->next == NULL)
+        return ;
+    prt_linked_list_recursive(node->next);
+}
+
+void    prt_linked_list(Node* node)
+{
+    Node*   current;
+    int i;
+
+    current = node;
+    i = 0;
+    while (current != NULL)
+    {
+        printf("Node %d: %d\n", i, current->value);
+        current = current->next;
+        i++;
+    }
+}
