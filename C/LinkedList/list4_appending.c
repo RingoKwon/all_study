12205a49d2e0426aaf730ada44e717c18b0a16d5
Ringo <Ringo.kwon@gmail.com>
2025-11-11 12:27:36 +0900

    revers almost done

--- C/LinkedList/list4_appending.c
+++ C/LinkedList/list4_appending.c
@@ -297,19 +297,21 @@ Node* reverse_list(Node* head)
     Node*   current;
     Node*   current_next;
     Node*   previous;
+    Node*   new_head;
 
     if (head == NULL)
         return (NULL);
     previous = NULL;
     current = head;
     current_next = head->next;
-    while (current->next->next != NULL)
+    while (current->next != NULL)
     {
         current->next = previous;
         previous = current;
+        new_head = current;
         current = current_next;
         current_next = current->next;
     }
-    return (current);
+    return (new_head);
 }
  
