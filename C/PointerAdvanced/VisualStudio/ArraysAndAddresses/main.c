1a89fc5ab27d858f53cf09a382113e0be7fa7e4d
Ringo <Ringo.kwon@gmail.com>
2026-02-05 15:24:54 +0900

    my versino

--- C/PointerAdvanced/VisualStudio/ArraysAndAddresses/main.c
+++ C/PointerAdvanced/VisualStudio/ArraysAndAddresses/main.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-	char str1[] = "Hello";
+	char str1[10] = "Hello";
 	char *str2 = "Goodbye";
-	// str2 = &str1;
-	// str2 = str1;
 	printf("%d %d %s\n", &str1, str1, str1);
 	printf("%d %d %s\n", &str2, str2, str2);
+	printf("-----pointer arithmetic-----\n");
+	printf("&str1 + 1 <- + 10 bytes: %d\n", &str1 + 1); // 
+	printf("str1 +1 <- 1 bytes: %d\n", str1 + 1 );
 	return 0;
 }
